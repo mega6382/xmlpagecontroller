835ae2dcb345ef163b844df25164123ebc3b45d1
roguevoo <roguevoo@04273aa8-0745-0410-b35b-8b76130e7852>
2009-08-28 18:43:01 +0000

    Somefixes

--- trunk/xml_page_controller/index.php
+++ trunk/xml_page_controller/index.php
@@ -1,34 +1,12 @@
 <?php
-<<<<<<< .mine
-	include "xmlpagecontroller.php"; // Include "XPC" library
-	$page = new xmlpage(		// Create a new instance of class xmlpage with given arguments.
-		'example.xml',		// Where to locate document with the content of pages.
-		"en",			// Language which will be displayed on the pages.
-		true			// Turn debugging information generation.
+
+	include "xmlpagecontroller.php";    // Include "XPC" library
+	$page = new xmlpage(                // Create a new instance of class xmlpage with given arguments.
+		'example.xml',                  // Where to locate document with the content of pages.
+		"en",                           // Language which will be displayed on the pages.
+		true                            // Turn debugging information generation.
 	);
-	echo $page->out();		// Output result to browser
-=======
 
-// Set language var
-$lang = isset( $_COOKIE['lang'] ) ? $_COOKIE['lang'] : 'en';
+	echo $page->out();                  // Output result to browser
 
-if( isset( $_GET['lang'] ) )
-{
-	$lang =  $_GET['lang'];
-	$url = isset( $_SERVER['HTTP_REFERER'] ) ? $_SERVER['HTTP_REFERER'] : '?';
-	setcookie('lang', $lang, time() + 3600 * 24, '/' );
-	header('Refresh: 1; url='.$url );
-	
-	echo 'Switch page language, please wait!';
-	exit;
-}
-
-require_once 'modules/xmlsite.php';
-// Create XML page controller instanse and parse file
-$site = new XML_site('example.xml', $lang, true );
-// Out result
-echo $site->out();
-// Out log if $_GET['log'] isset
-if( isset( $_GET['log'] ) ) echo $site->log_print();
->>>>>>> .r14
 ?>
