0c46f39aa68b7934133d57c1ee7c494218e9f571
roguevoo <roguevoo@04273aa8-0745-0410-b35b-8b76130e7852>
2009-09-09 08:11:33 +0000

    Spleet implementations

--- trunk/xpc5/SiteGeneratorFactory.php
+++ trunk/xpc5/SiteGeneratorFactory.php
@@ -1,138 +1,101 @@
 <?php
 
-    class tagPagesGenerator extends SitemapGenerator
+/**
+ * To create a generator class must inherit from this class as a base
+ */
+abstract class SitemapGenerator extends NodeProcessor
+{
+    /**
+     * Do generation process
+     * @param DOMNode $node
+     */
+    abstract public function generate( DOMNode & $node );
+
+    /**
+     * Get element tag name where apply this generator
+     */
+    abstract public function name();
+}
+
+class tagPagesGenerator extends SitemapGenerator
+{
+    public function name() {
+        return 'pages';
+    }
+
+    public function generate( DOMNode & $node )
     {
-        public function name() {
-            return 'pages';
+        if( !$node )
+        return null;
+
+        $params = array();
+
+        foreach( $node->child() as $item )
+        {
+        if( $item->name() != 'param' )
+            continue;
+
+        $name = $item->attr('name');
+
+        if( $name == null )
+            continue;
+
+        $params[ $name ] = $item->data();
         }
 
-        public function generate( DOMNode & $node )
-	{
-	    if( !$node )
-		return null;
+        if( count($params) < 1 )
+        return null;
 
-	    $params = array();
+        $result = array();
 
-	    foreach( $node->child() as $item )
-	    {
-		if( $item->name() != 'param' )
-		    continue;
-
-		$name = $item->attr('name');
-
-		if( $name == null )
-		    continue;
-
-		$params[ $name ] = $item->data();
-	    }
-
-	    if( count($params) < 1 )
-		return null;
-
-	    $result = array();
-
-	    foreach( $params as $key => $val )
-	    {
-		$n = $node->document()->createElement('page');
-		$n->attr( 'id', $val );
-		array_push($result, $n);
-	    }
-	    return $result;
-	}
-    }
-
-    abstract class DataCollection
-    {
-        private $m_data = array();
-        public function has( $key )
-        { return key_exists($key, $this->m_data); }
-
-        public function get( $key )
-        { return $this->has($key) ? $this->m_data[$key] : null; }
-
-        public function set( $key, $val )
-        { $this->m_data[$key] = $val; }
-
-        public function del( $key )
-        { unset( $this->m_data[ $key ] ); }
-    }
-
-    class SitemapParserFactory extends DataCollection
-    {
-        static private $m_instance = null;
-        
-        private function __construct()
+        foreach( $params as $key => $val )
         {
-            foreach( get_declared_classes() as $decl )
+        $n = $node->document()->createElement('page');
+        $n->attr( 'id', $val );
+        array_push($result, $n);
+        }
+        return $result;
+    }
+}
+
+class SitemapGeneratorFactory extends DataCollection
+{
+    static private $m_instance = null;
+
+    private function __construct()
+    {
+        foreach( get_declared_classes() as $decl )
+        {
+            if( !is_subclass_of($decl, 'SitemapGenerator') ) continue;
+
+
+            $instance = new $decl();
+            $declname = $instance->name();
+
+            if( !$declname || !is_string($declname) || !strlen($declname) || $declname == 'undefined_parser' )
+                continue;
+
+            $keys = explode( ' ', $declname );
+
+            foreach( $keys as $k )
             {
-                if( !is_subclass_of($decl, 'SitemapParser') ) continue;
+                $k = trim($k);
 
-                $instance = new $decl();
-                $declname = $instance->name();
+                if( !$k  ) continue;
 
-                if( !$declname || !is_string($declname) || !strlen($declname) || $declname == 'undefined_parser' )
-                    continue;
-
-                $keys = explode( ' ', $declname );
-
-                foreach( $keys as $k )
-                {
-                    $k = trim($k);
-                    if( !$k  ) continue;
-
-                    $this->set($k, $instance);
-                }
+                $this->set($k, $instance);
             }
         }
-
-        private function __clone(){}
-        static public function instance()
-        {
-            if( self::$m_instance === null )
-                self::$m_instance = new SitemapParserFactory();
-
-            return self::$m_instance;
-        }
     }
-    
-    class SitemapGeneratorFactory extends DataCollection
+
+    private function __clone(){}
+    static public function instance()
     {
-        static private $m_instance = null;
- 
-        private function __construct()
-        {
-            foreach( get_declared_classes() as $decl )
-            {
-                if( !is_subclass_of($decl, 'SitemapGenerator') ) continue;
+        if( self::$m_instance === null )
+            self::$m_instance = new SitemapGeneratorFactory();
 
-		
-                $instance = new $decl();
-                $declname = $instance->name();
-
-                if( !$declname || !is_string($declname) || !strlen($declname) || $declname == 'undefined_parser' )
-                    continue;
-
-                $keys = explode( ' ', $declname );
-
-                foreach( $keys as $k )
-                {
-                    $k = trim($k);
-
-                    if( !$k  ) continue;
-
-		    $this->set($k, $instance);
-                }
-            }
-        }
-
-        private function __clone(){}
-        static public function instance()
-        {
-            if( self::$m_instance === null )
-                self::$m_instance = new SitemapGeneratorFactory();
-
-            return self::$m_instance;
-        }
+        return self::$m_instance;
     }
+}
 
 ?>
--- trunk/xpc5/SiteParserFactory.php
+++ trunk/xpc5/SiteParserFactory.php
@@ -1,89 +1,139 @@
 <?php
 
-    class tagRecursiveParser extends SitemapParser
-    {
-        function name()
-        {
-            return 'site root config conf metas options';
-        }
+/**
+ * SitemapParser abstract class
+ *
+ * To create a parser class must inherit from this class as a base
+ *
+ */
+abstract class SitemapParser extends NodeProcessor
+{
+    abstract public function parse( DOMNode & $node );
+    abstract public function name();
+}
 
-        function parse( DOMNode & $node )
+class tagRecursiveParser extends SitemapParser
+{
+    function name()
+    {
+        return 'site root config conf metas options';
+    }
+
+    function parse( DOMNode & $node )
+    {
+        $this->parent->parse_recursive($node);
+    }
+}
+
+class tagOptionParser extends SitemapParser
+{
+    function name()
+    {
+        return 'option';
+    }
+
+    function parse( DOMNode & $node )
+    {
+        $this->parent->addOption( $node->attr('name'), $node->data() );
+    }
+}
+
+class tagOptionsParser extends SitemapParser
+{
+    function name()
+    {
+        return 'options';
+    }
+
+    function parse( DOMNode & $node )
+    {
+        foreach( $node->child() as $n )
+            $this->parent->addOption( $n->name(), $n->data() );
+    }
+}
+
+class tagMetasParser extends SitemapParser
+{
+    function name()
+    {
+        return 'metas';
+    }
+
+    function parse( DOMNode & $node )
+    {
+        foreach( $node->child() as $n )
+            $this->parent->addMeta( $n->name(), $n->data() );
+    }
+}
+
+class tagMetaParser extends SitemapParser
+{
+    function name()
+    {
+        return 'meta';
+    }
+
+    function parse( DOMNode & $node )
+    {
+        $this->parent->addMeta( $node->attr('name'), $node->data() );
+    }
+}
+
+class tagPageParser extends SitemapParser
+{
+    var $stack = array();
+
+    function name(){ return 'page'; }
+
+    function parse( DOMNode & $node )
+    {
+        $id = $node->find_value('id');
+
+        array_push($this->stack, $id );
+        $path = implode('/', $this->stack );
+        $this->parent->parse_recursive($node);
+        array_pop($this->stack);
+
+        $this->parent->addPage( $path, $node );
+    }
+}
+
+class SitemapParserFactory extends DataCollection
+{
+    static private $m_instance = null;
+
+    private function __construct()
+    {
+        foreach( get_declared_classes() as $decl )
         {
-            $this->parent->parse_recursive($node);
+            if( !is_subclass_of($decl, 'SitemapParser') ) continue;
+
+            $instance = new $decl();
+            $declname = strtolower( $instance->name() );
+
+            if( !$declname || !is_string($declname) || !strlen($declname) || $declname == 'undefined_parser' )
+                continue;
+
+            $keys = explode( ' ', $declname );
+
+            foreach( $keys as $k )
+            {
+                $k = trim( $k );
+                if( !$k  ) continue;
+
+                $this->set($k, $instance);
+            }
         }
     }
 
-    class tagOptionParser extends SitemapParser
+    private function __clone(){}
+    static public function instance()
     {
-        function name()
-        {
-            return 'option';
-        }
+        if( self::$m_instance === null )
+            self::$m_instance = new SitemapParserFactory();
 
-        function parse( DOMNode & $node )
-        {
-            $this->parent->addOption( $node->attr('name'), $node->data() );
-        }
+        return self::$m_instance;
     }
-
-    class tagOptionsParser extends SitemapParser
-    {
-        function name()
-        {
-            return 'options';
-        }
-
-        function parse( DOMNode & $node )
-        {
-            foreach( $node->child() as $n )
-                $this->parent->addOption( $n->name(), $n->data() );
-        }
-    }
-
-    class tagMetasParser extends SitemapParser
-    {
-        function name()
-        {
-            return 'metas';
-        }
-
-        function parse( DOMNode & $node )
-        {
-            foreach( $node->child() as $n )
-                $this->parent->addMeta( $n->name(), $n->data() );
-        }
-    }
-
-    class tagMetaParser extends SitemapParser
-    {
-        function name()
-        {
-            return 'meta';
-        }
-
-        function parse( DOMNode & $node )
-        {
-            $this->parent->addMeta( $node->attr('name'), $node->data() );
-        }
-    }
-
-    class tagPageParser extends SitemapParser
-    {
-        var $stack = array();
-
-        function name(){ return 'page'; }
-
-        function parse( DOMNode & $node )
-        {
-            $id = $node->find_value('id');
-
-            array_push($this->stack, $id );
-            $path = implode('/', $this->stack );
-            $this->parent->parse_recursive($node);
-            array_pop($this->stack);
-
-            $this->parent->addPage( $path, $node );
-        }
-    }
+}
 
 ?>
