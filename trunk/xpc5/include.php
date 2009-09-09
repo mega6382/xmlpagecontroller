f48e5d75c2f374c1bb47bc72746c85919de4a1f7
roguevoo <roguevoo@04273aa8-0745-0410-b35b-8b76130e7852>
2009-09-09 08:24:45 +0000

    All in one update

--- trunk/xpc5/include.php
+++ trunk/xpc5/include.php
@@ -1,14 +1,18 @@
 <?php
 
     /** Define XPC system include path */
-    define( 'XPC_CLASS_SYSTEM_PATH', dirname(__FILE__) . '/' );
+    define( 'XPC_CLASS_SYSTEM_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR );
 
     /** Include classes */
+    require_once XPC_CLASS_SYSTEM_PATH . 'Template.class.php';
     require_once XPC_CLASS_SYSTEM_PATH . 'XMLTag.class.php';
     require_once XPC_CLASS_SYSTEM_PATH . 'XMLParser.class.php';
     require_once XPC_CLASS_SYSTEM_PATH . 'XMLConfig.class.php';
     require_once XPC_CLASS_SYSTEM_PATH . 'ZIPReader.class.php';
     require_once XPC_CLASS_SYSTEM_PATH . 'XMLPage.class.php';
     require_once XPC_CLASS_SYSTEM_PATH . 'XMLSite.class.php';
+    require_once XPC_CLASS_SYSTEM_PATH . 'DOMMutator.class.php';
+    require_once XPC_CLASS_SYSTEM_PATH . 'DOMTransformer.class.php';
+    require_once XPC_CLASS_SYSTEM_PATH . 'DOMSite.class.php';
 
 ?>
