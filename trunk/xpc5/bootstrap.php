15836f9c8f14e090ac53ecd740ebb5b9e367e356
roguevoo <roguevoo@04273aa8-0745-0410-b35b-8b76130e7852>
2009-09-15 07:00:26 +0000

    Add some includes

--- trunk/xpc5/bootstrap.php
+++ trunk/xpc5/bootstrap.php
@@ -13,13 +13,16 @@
 */
 
 
-
     if( !defined('XPC_CLASSES') ) define('XPC_CLASSES',1);
 
-    /** Define XPC system include path */
+    /*
+     *  Define XPC system include path
+     */
     define( 'XPC_CLASS_SYSTEM_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR );
 
-    /** Include classes */
+    /*
+     * Include classes
+     */
     require XPC_CLASS_SYSTEM_PATH . 'Template.class.php';
     require XPC_CLASS_SYSTEM_PATH . 'XMLTag.class.php';
     require XPC_CLASS_SYSTEM_PATH . 'XMLParser.class.php';
@@ -31,8 +34,13 @@
     require XPC_CLASS_SYSTEM_PATH . 'DOMMutator.class.php';
     require XPC_CLASS_SYSTEM_PATH . 'DOMParser.class.php';
     require XPC_CLASS_SYSTEM_PATH . 'DOMDataBase.class.php';
+    require XPC_CLASS_SYSTEM_PATH . 'DOMDictonary.class.php';
     require XPC_CLASS_SYSTEM_PATH . 'DOMPart.class.php';
     require XPC_CLASS_SYSTEM_PATH . 'HTMLPart.class.php';
+    require XPC_CLASS_SYSTEM_PATH . 'DOMRole.class.php';
+    require XPC_CLASS_SYSTEM_PATH . 'Role.class.php';
     require XPC_CLASS_SYSTEM_PATH . 'DOMSite.class.php';
+    require XPC_CLASS_SYSTEM_PATH . 'Document.class.php';
+    require XPC_CLASS_SYSTEM_PATH . 'Application.class.php';
 
 ?>
